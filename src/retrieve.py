a4d41c364d3f33b795042758447e5817dc83c683
HoangTM1324 <thachminhhoang132004@gmail.com>
2026-02-28 16:04:00 +0700

    Add retrieved function.

--- src/retrieve.py
+++ src/retrieve.py
@@ -1,14 +1,12 @@
 import getpass
 import os
-import hashlib
+import chromadb
 from dotenv import load_dotenv
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_core.messages import SystemMessage, HumanMessage
 import arxiv
 from langchain_core.documents import Document
 from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_community.document_loaders import PyMuPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from .schema import GraphState
 load_dotenv()
 # embeddings = GoogleGenerativeAIEmbeddings(model="models/gemini-embedding-001")
@@ -16,6 +14,13 @@ llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0)
 
+def retrieve_from_chromadb_node(state: GraphState):
+    print("--- RETRIEVING FROM DATABASE ---")
+    client= chromadb.Client()
+    collection= client.get_or_create_collection(name="pdf_chunks")
+    query= state["question"]
+    results= collection.query(query_texts= query, n_results= 3)
+    return results["documents"][0]
 
 def generate_web_subquestions_node(state: GraphState):
     user_input = state["question"]
@@ -43,46 +48,8 @@ def generate_web_subquestions_node(state: GraphState):
     return content
 
 
-def process_local_pdf_node(state: GraphState):
-    print("--- PROCESSING LOCAL PDF ---")
-    file_path = state["file_path"]
-
-    
-    loader= PyMuPDFLoader(file_path)
-    data= loader.load()
-    system_prompt= """You are an expert academic search assistant. Your task is to analyze the content of the PDF document and find out the title and authors of the article. Your output should return a string in the format of "title \n Authors" """
-    # print(data[0].page_content)
-    message = [
-        SystemMessage(content= system_prompt),
-        HumanMessage(content= f"PDF CONTENT: \n{data[0].page_content}")
-    ]
-    pdf_info= llm.invoke(message)
-    pdf_info= pdf_info.content.strip().split("\n")
-
-    for page in data:
-        page.metadata["title"]= pdf_info[0]
-        page.metadata["author"]= pdf_info[1]
-
-    text_splitter= RecursiveCharacterTextSplitter(chunk_size= 2000, chunk_overlap= 100)
-
-    chunks= text_splitter.split_documents(data)
-    chunk_ids = []
-    for c in chunks:
-        # Tạo dấu vân tay dựa trên nội dung đoạn văn
-        content_hash = hashlib.md5(c.page_content.encode()).hexdigest()
-        # ID = tên_file + số_trang + mã_hash
-        source_name = c.metadata.get("source", "unknown").split("\\")[-1] # Lấy tên file gọn
-        page_num = c.metadata.get("page", 0)
-        chunk_id = f"{source_name}_p{page_num}_{content_hash}"
-        chunk_ids.append(chunk_id)
-
-    print(f"--- Created {len(chunks)} chunks with unique IDs ---")
-
-    # Trả về documents kèm ids để bước sau nạp vào ChromaDB
-    return {"documents": chunks, "ids": chunk_ids}
-
 def web_search_node(content):
-    print("---Web Search---")
+    print("--- Web Search ---")
     web_search_tool = TavilySearchResults(k=3)
     web_results= []
     state= []
